bcad00026e5e3496e187fbf16a3bce46705a3155
Luís Silva <90908115+LuiSilvak@users.noreply.github.com>
2025-04-12 19:15:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,8 @@
 document.querySelectorAll('.tab').forEach(button => {
   button.addEventListener('click', () => {
     document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
-    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
-
     button.classList.add('active');
-    document.getElementById(button.dataset.tab).classList.add('active');
-  });
-});
 
-
-
-document.querySelectorAll('.filtros button').forEach(button => {
-  button.addEventListener('click', () => {
-    document.querySelectorAll('.filtros button').forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
     const nivel = button.dataset.nivel;
 
     document.querySelectorAll('.card').forEach(card => {
