f13b24779a7254473599ffb0a1540ab0ed14d171
Luís Silva <90908115+LuiSilvak@users.noreply.github.com>
2025-04-12 15:40:56 -0300

    Atualizando script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
-// Espera o DOM estar carregado
+
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".nav-btn");
   const sections = document.querySelectorAll(".content");
 
   buttons.forEach(button => {
     button.addEventListener("click", () => {
-      // Remove active de todos os botões e seções
       buttons.forEach(btn => btn.classList.remove("active"));
       sections.forEach(sec => sec.classList.remove("active"));
 
-      // Adiciona active ao botão clicado
       button.classList.add("active");
 
-      // Mostra a seção correspondente
       const page = button.getAttribute("data-page");
       const targetSection = document.getElementById(page);
       if (targetSection) {
@@ -21,4 +18,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
