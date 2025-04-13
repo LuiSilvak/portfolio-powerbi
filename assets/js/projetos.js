62b0211d1558d2a2ff00fe96f262a1250ed6f92e
Luís Silva <90908115+LuiSilvak@users.noreply.github.com>
2025-04-13 16:49:36 -0300

    embed

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -6,6 +6,7 @@ const projetos = [
     descricao_en: "Basic sales indicators segmented by period.",
     imagem: "./assets/images/vendas-simples.jpg",
     link: "https://app.powerbi.com/view?r=eyJrIjoiZmZkMDhiNWMtNTRiNS00ZGFkLTk3MDAtODM4NmZjZWU0MmJlIiwidCI6ImVjNmU0N2QyLWEwNGMtNGQ2Yy1iZjhmLWQ4ZWQyNzg1NzQ5YiJ9",
+    embed: "https://app.powerbi.com/view?r=eyJrIjoiZmZkMDhiNWMtNTRiNS00ZGFkLTk3MDAtODM4NmZjZWU0MmJlIiwidCI6ImVjNmU0N2QyLWEwNGMtNGQ2Yy1iZjhmLWQ4ZWQyNzg1NzQ5YiJ9",
     nivel: "iniciante",
     destaque: true
   },
